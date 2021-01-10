c06faf2ce37af2efc389cbbc1ab4fc134d7e8640
Hmerman6006 <hh8686@yandex.com>
2021-01-10 14:50:51 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from kivymd.app import MDApp
+from views.rootlay import RootLay
+
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.primary_palette = "Green"
+        self.theme_cls.primary_hue = "500"
+        self.theme_cls.theme_style = "Light"
+
+
+MainApp().run()
